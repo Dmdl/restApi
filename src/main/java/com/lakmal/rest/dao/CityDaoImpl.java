31ed1e0ca910d7604e84c09a648e2a8af5471eb8
lakmal <lakmal@rcapl.com>
2015-03-06 11:42:09 +0530

    Add dao s autowire support

--- src/main/java/com/lakmal/rest/dao/CityDaoImpl.java
+++ src/main/java/com/lakmal/rest/dao/CityDaoImpl.java
@@ -2,18 +2,31 @@ package com.lakmal.rest.dao;
 
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.lakmal.rest.model.City;
 
-public class CityDaoImpl extends JdbcDaoSupport implements CityDao {
+@Repository
+public class CityDaoImpl implements CityDao {
+
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
 	@Override
 	public List<City> getCityiesForCountry(String code) {
 		String sql = "SELECT * FROM world.City where CountryCode = ?";
-		List<City> countries = (List<City>) getJdbcTemplate().query(sql,
-				new Object[] { code }, new BeanPropertyRowMapper(City.class));
+		List<City> countries = (List<City>) jdbcTemplate.query(sql,
+				new Object[] { code }, new BeanPropertyRowMapper<City>(
+						City.class));
 
 		return countries;
 	}
--- src/main/java/com/lakmal/rest/dao/CountryDaoImpl.java
+++ src/main/java/com/lakmal/rest/dao/CountryDaoImpl.java
@@ -2,25 +2,37 @@ package com.lakmal.rest.dao;
 
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.lakmal.rest.model.Country;
 
-public class CountryDaoImpl extends JdbcDaoSupport implements CountryDao {
+@Repository
+public class CountryDaoImpl implements CountryDao {
+
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
 	public List<Country> getCountries() {
 		String sql = "SELECT * FROM Country";
-		List<Country> countries = (List<Country>) getJdbcTemplate().query(sql,
-				new BeanPropertyRowMapper(Country.class));
+		List<Country> countries = (List<Country>) jdbcTemplate.query(sql,
+				new BeanPropertyRowMapper<Country>(Country.class));
 		return countries;
 	}
 
 	public Country getCountry(String code) {
 		String sql = "SELECT * FROM Country WHERE Code=?";
-		Country country = (Country) getJdbcTemplate()
-				.queryForObject(sql, new Object[] { code },
-						new BeanPropertyRowMapper(Country.class));
+		Country country = (Country) jdbcTemplate.queryForObject(sql,
+				new Object[] { code }, new BeanPropertyRowMapper<Country>(
+						Country.class));
 		return country;
 	}
 
