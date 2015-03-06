003e16830070f37aa29e8ddb28633c4171def60f
lakmal <lakmal@rcapl.com>
2015-03-06 12:05:23 +0530

    Add log

--- src/main/java/com/lakmal/rest/controller/CountryController.java
+++ src/main/java/com/lakmal/rest/controller/CountryController.java
@@ -2,6 +2,8 @@ package com.lakmal.rest.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,20 +16,25 @@ import com.lakmal.rest.model.Country;
 import com.lakmal.rest.service.CountryService;
 
 @Controller
+@RequestMapping("/v1")
 public class CountryController {
 
 	@Autowired
 	private CountryService countryService;
 
+	Logger LOG = LoggerFactory.getLogger(getClass());
+
 	@RequestMapping(value = "/countries", produces = "application/json", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Country> getCountries() {
+		LOG.debug("Accessing get country list service");
 		return countryService.getCountries();
 	}
 
 	@RequestMapping(value = "/country/{code}", produces = "application/json", method = RequestMethod.GET)
 	@ResponseBody
 	public Country getCountry(@PathVariable String code) {
+		LOG.debug("Accessing get country service with country code " + code);
 		return countryService.getCountry(code);
 	}
 
